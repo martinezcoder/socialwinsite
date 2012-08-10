0736217acdf4342489f4711899c2109da9c0d854
socialwi <kundafran@gmail.com>
2012-08-10 14:10:26 +0200

    bd optimizada, imagenes arregladas

--- wp-content/themes/theme1312/comments.php
+++ wp-content/themes/theme1312/comments.php
@@ -70,7 +70,7 @@
 
 <!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
 
-<p>Mensage<br /><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea></p>
+<p>Mensaje<br /><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea></p>
 
 <p><input name="submit" type="submit" id="submit" tabindex="5" value="Insertar comentario" />
 <?php comment_id_fields(); ?>
