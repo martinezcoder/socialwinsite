56e860524535109083f6a88c7fb05476fdbc5dc3
Fran LP <fran.martinez@socialwin.es>
2012-12-07 12:38:32 +0100

    gestion remotes

--- BACKUPDB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#export PATH=$PATH:/d/xampp/mysql/bin
-
-mysqldump --opt --user=socialwi_admin --password='Velez12$' socialwi_wordpress --ignore-table=socialwi_wordpress.wp_comments --ignore-table=socialwi_wordpress.wp_commentmeta > socialwin.sql
